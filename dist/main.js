03b23329f1f77bada5b7e483e6ff64ad6f2c8ca7
Yilmer Daniel Avila Villarreal <yavila@abatechnology.com>
2023-08-29 16:58:18 -0500

    Fix cors

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,7 @@ const common_1 = require("@nestjs/common");
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     app.use((0, helmet_1.default)());
     await app.listen(port);
     common_1.Logger.log(`Running in port ${port}`, 'main');

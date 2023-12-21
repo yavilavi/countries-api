a88f2c8b8946205374c9cc8585496e4bcf9a22c5
Yilmer Daniel Avila Villarreal <yavila@abatechnology.com>
2023-12-21 16:00:47 -0500

    fix: linked in URl

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,7 +8,7 @@ export class AppService {
       <h1>Countries API</h1>
       <p>A basic API for getting countries and cities list</p>
       <small>Created by Yilmer Avila</small><br><br>
-      <a href="linkedin.com/in/yilmeravila/" target="_blank">LinkedIn</a>
+      <a href="https://www.linkedin.com/in/yilmeravila/" target="_blank">LinkedIn</a>
     </div>
     `;
   }
